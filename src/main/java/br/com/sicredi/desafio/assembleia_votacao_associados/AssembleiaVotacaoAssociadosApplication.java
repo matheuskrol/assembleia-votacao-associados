df75f919ce5355f46d2f8f8371e43df9ca394699
matheuskrol <matheuskrol27@gmail.com>
2024-11-02 17:12:14 -0300

    Primeiro commit

---
+++ src/main/java/br/com/sicredi/desafio/assembleia_votacao_associados/AssembleiaVotacaoAssociadosApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.desafio.assembleia_votacao_associados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssembleiaVotacaoAssociadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssembleiaVotacaoAssociadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sicredi/desafio/assembleia_votacao_associados/AssembleiaVotacaoAssociadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.desafio.assembleia_votacao_associados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssembleiaVotacaoAssociadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
